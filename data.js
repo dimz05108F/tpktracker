669c9c81b9285f1dfb3e02f88fad55d9fcc1acaf
dimz05108F <dimz05008@gmail.com>
2025-05-16 21:59:15 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,546 @@
+const jadwalKereta = [
+    {
+      "nomor_ka": "2203",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "05:30",
+        "05:38",
+        "05:42",
+        "05:46"
+      ]
+    },
+    {
+      "nomor_ka": "2205",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "06:00",
+        "06:08",
+        "06:12",
+        "06:16"
+      ]
+    },
+    {
+      "nomor_ka": "2207",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "06:30",
+        "06:38",
+        "06:42",
+        "06:46"
+      ]
+    },
+    {
+      "nomor_ka": "2209",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "07:00",
+        "07:08",
+        "07:12",
+        "07:16"
+      ]
+    },
+    {
+      "nomor_ka": "2211",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "07:30",
+        "07:38",
+        "07:42",
+        "07:46"
+      ]
+    },
+    {
+      "nomor_ka": "2213",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "08:00",
+        "08:08",
+        "08:12",
+        "08:16"
+      ]
+    },
+    {
+      "nomor_ka": "2215",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "08:30",
+        "08:38",
+        "08:42",
+        "08:46"
+      ]
+    },
+    {
+      "nomor_ka": "2249",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "17:00",
+        "17:08",
+        "17:12",
+        "17:16"
+      ]
+    },
+    {
+      "nomor_ka": "2251",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "17:30",
+        "17:38",
+        "17:42",
+        "17:46"
+      ]
+    },
+    {
+      "nomor_ka": "2253",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "18:00",
+        "18:08",
+        "18:12",
+        "18:16"
+      ]
+    },
+    {
+      "nomor_ka": "2255",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "18:30",
+        "18:38",
+        "18:42",
+        "18:46"
+      ]
+    },
+    {
+      "nomor_ka": "2257",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "19:00",
+        "19:08",
+        "19:12",
+        "19:16"
+      ]
+    },
+    {
+      "nomor_ka": "2259",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "19:30",
+        "19:38",
+        "19:42",
+        "19:46"
+      ]
+    },
+    {
+      "nomor_ka": "2261",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "20:00",
+        "20:08",
+        "20:12",
+        "20:16"
+      ]
+    },
+    {
+      "nomor_ka": "2263",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "20:30",
+        "20:38",
+        "20:42",
+        "20:46"
+      ]
+    },
+    {
+      "nomor_ka": "2265",
+      "relasi": "TPK-JAKK",
+      "arah": "TPK \u2192 JAKK",
+      "stasiun": [
+        "Tanjung Priuk",
+        "Ancol",
+        "Kampung Bandan",
+        "Jakarta Kota"
+      ],
+      "waktu": [
+        "21:00",
+        "21:08",
+        "21:12",
+        "21:16"
+      ]
+    },
+    {
+      "nomor_ka": "2200",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "05:00",
+        "05:04",
+        "05:07",
+        "05:16"
+      ]
+    },
+    {
+      "nomor_ka": "2202",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "05:30",
+        "05:34",
+        "05:37",
+        "05:46"
+      ]
+    },
+    {
+      "nomor_ka": "2204",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "06:00",
+        "06:04",
+        "06:07",
+        "06:16"
+      ]
+    },
+    {
+      "nomor_ka": "2206",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "06:30",
+        "06:34",
+        "06:37",
+        "06:46"
+      ]
+    },
+    {
+      "nomor_ka": "2208",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "07:00",
+        "07:04",
+        "07:07",
+        "07:16"
+      ]
+    },
+    {
+      "nomor_ka": "2210",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "07:30",
+        "07:34",
+        "07:37",
+        "07:46"
+      ]
+    },
+    {
+      "nomor_ka": "2212",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "08:00",
+        "08:04",
+        "08:07",
+        "08:16"
+      ]
+    },
+    {
+      "nomor_ka": "2246",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "16:30",
+        "16:34",
+        "16:37",
+        "16:46"
+      ]
+    },
+    {
+      "nomor_ka": "2248",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "17:00",
+        "17:04",
+        "17:07",
+        "17:16"
+      ]
+    },
+    {
+      "nomor_ka": "2250",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "17:30",
+        "17:34",
+        "17:37",
+        "17:46"
+      ]
+    },
+    {
+      "nomor_ka": "2252",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "18:00",
+        "18:04",
+        "18:07",
+        "18:16"
+      ]
+    },
+    {
+      "nomor_ka": "2254",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "18:30",
+        "18:34",
+        "18:37",
+        "18:46"
+      ]
+    },
+    {
+      "nomor_ka": "2256",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "19:00",
+        "19:04",
+        "19:07",
+        "19:16"
+      ]
+    },
+    {
+      "nomor_ka": "2258",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "19:30",
+        "19:34",
+        "19:37",
+        "19:46"
+      ]
+    },
+    {
+      "nomor_ka": "2260",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "20:00",
+        "20:04",
+        "20:07",
+        "20:16"
+      ]
+    },
+    {
+      "nomor_ka": "2262",
+      "relasi": "JAKK-TPK",
+      "arah": "JAKK \u2192 TPK",
+      "stasiun": [
+        "Jakarta Kota",
+        "Kampung Bandan",
+        "Ancol",
+        "Tanjung Priuk"
+      ],
+      "waktu": [
+        "20:30",
+        "20:34",
+        "20:37",
+        "20:46"
+      ]
+    }
+  ];
